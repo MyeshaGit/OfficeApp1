3b08640ddaabc50d5c98417edb887e35ad5a2399
myrice <myrice@MYRICE-W540>
2017-08-08 06:49:29 -0400

    Add project files.

---
+++ OfficeApp1Web/AppRead/App.js
@@ -0,0 +1,87 @@
+﻿/* Common app functionality */
+
+var app = (function () {
+    "use strict";
+
+    var app = {};
+
+    // Common initialization function (to be called from each page)
+    app.initialize = function () {
+        $('body').append(
+            '<div id="notification-message">' +
+                '<div class="padding">' +
+                    '<div id="notification-message-close"></div>' +
+                    '<div id="notification-message-header"></div>' +
+                    '<div id="notification-message-body"></div>' +
+                '</div>' +
+            '</div>');
+
+        $('#notification-message-close').click(function () {
+            $('#notification-message').hide();
+        });
+
+
+        // After initialization, expose a common notification function
+        app.showNotification = function (header, text) {
+            $('#notification-message-header').text(header);
+            $('#notification-message-body').text(text);
+            $('#notification-message').slideDown('fast');
+        };
+    };
+
+    $(function () {
+        // Get the form.
+        var form = $('#ajax-contact');
+
+        // Get the messages div.
+        var formMessages = $('#form-messages');
+
+        // TODO: The rest of the code will go here...
+    });
+
+    // Set up an event listener for the contact form.
+    $(form).submit(function (event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+
+        // TODO
+    });
+
+
+            
+    // Serialize the form data.
+    var formData = $(form).serialize();
+
+    // Submit the form using AJAX.
+    $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+    }).done(function (response) {
+        // Make sure that the formMessages div has the 'success' class.
+        $(formMessages).removeClass('error');
+        $(formMessages).addClass('success');
+
+        // Set the message text.
+        $(formMessages).text(response);
+
+        // Clear the form.
+        $('#name').val('');
+        $('#email').val('');
+        $('#message').val('');
+    }).fail(function (data) {
+        // Make sure that the formMessages div has the 'error' class.
+        $(formMessages).removeClass('success');
+        $(formMessages).addClass('error');
+
+        // Set the message text.
+        if (data.responseText !== '') {
+            $(formMessages).text(data.responseText);
+        } else {
+            $(formMessages).text('Oops! An error occured and your message could not be sent.');
+        }
+    });
+
+
+    return app;
+})();
